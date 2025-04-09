e87daf2aab0587a38e0ac1af539dcd4386de09f9
aliounebalde <alioune.balde@edu.univ-paris13.fr>
2025-04-09 09:55:08 +0200

    FEAT: Add thermostat API

---
+++ app/models/thermostat.py
@@ -0,0 +1,46 @@
+from pydantic import BaseModel, Field
+from ..utils.mode import ThermostatMode
+import time
+
+
+class ThermostatController():
+    def __init__(self):
+        self.current_temperature: float | None
+        self.target_temperature: float
+        self.mode: ThermostatMode = ThermostatMode.AUTO
+        self.state: str
+        self.last_state_change = time.time()
+        self.min_cycle_duration = 300
+
+    def get_status(self):
+        """Get the current status of the thermostat."""
+        return {
+            "current_temperature": self.current_temperature,
+            "target_temperature": self.target_temperature,
+            "mode": self.mode,
+            "state": self.state
+        }
+
+    def set_target_temp(self, temperature: float):
+        """Set the target temperature."""
+        self.target_temperature = temperature
+
+    def set_mode(self, mode: ThermostatMode):
+        """Set the mode of the thermostat."""
+        self.mode = mode
+
+    def evaluate(self):
+        now = time.time()
+        if self.current_temperature is None:
+            return
+
+        # respect du min_cycle_duration
+        if now - self.last_state_change < self.min_cycle_duration:
+            return
+
+        if self.current_temperature < self.target_temperature - self.cold_tolerance:
+            self.state = "on"
+            self.last_state_change = now
+        elif self.current_temperature > self.target_temperature + self.hot_tolerance:
+            self.state = "off"
+            self.last_state_change = now
---
+++ app/utils/mode.py
@@ -0,0 +1,15 @@
+# An mode Enum class for a thermostat controller.
+# This class defines the different modes that a thermostat can be in.
+
+from enum import Enum
+
+
+class ThermostatMode(Enum):
+    """Enum class for thermostat modes."""
+
+    AUTO = "auto"
+    MANUAL = "manual"
+    OFF = "off"
+
+    def __str__(self):
+        return self.value
